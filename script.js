9eb01beaaf6b79c13895df8203f4c8db3623e4d9
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-23 16:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const Btn = document.querySelector("#enterBtn");
+const para = document.querySelector("#status");
+
+Btn.addEventListener(('click'), () =>{
+	if(para.textContent.length > 0){
+		para.textContent = '';
+	}
+	const heading = document.createElement("h1");
+	heading.textContent = "Entered Metaverse";
+	para.appendChild(heading);
+});
